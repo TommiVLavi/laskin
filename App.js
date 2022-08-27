1e9819fecf365d8b9bc967e54bfc1774d1dfd70f
TommiVLavi <bgr912@myy.haaga-helia.fi>
2022-08-27 22:41:53 +0300

    Teht 2 (kesken)

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
+  const [numb, setNumb] = useState(0);
+  const [numbKaks, setNumbKaks] = useState(0);
+  const [tulos, setTulos] = useState(0);
+
+  const plus = () => {
+    setTulos(numb + numbKaks)
+  }
+
+  const miinus = () => {
+    setTulos(numb - numbKaks)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.test}>
+
+      <View >
+        <Text>Tulos: {tulos}</Text>
+      </View>
+      
+      <View style={styles.input}>
+        <TextInput style={styles.input} onChange={numb => setNumb(numb)} value={numb}/>
+
+        <TextInput style={styles.input} onChangeText={numbKaks => setNumbKaks(numbKaks)} value={numbKaks}/>
+      </View>
+
+      <View>
+        <Button title="Plussa" style={styles.button} onPress={plus}/>
+
+        <Button title="Miinus" style={styles.button} onPress={miinus}/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  test: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  
+  input: {
+    width: 70,
+    borderColor: 'green',
+    borderWidth: 2,
   },
 });
