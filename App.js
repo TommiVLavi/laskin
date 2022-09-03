8e4c39d1b7a30e36d9807c74f57a7a49ade9855f
TommiVLavi <bgr912@myy.haaga-helia.fi>
2022-09-03 21:36:57 +0300

    Teht 1 korjattu

--- App.js
+++ App.js
@@ -1,44 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
   const [numb, setNumb] = useState(0);
   const [numbKaks, setNumbKaks] = useState(0);
   const [tulos, setTulos] = useState(0);
+  const initialFocus = useRef(null);
 
-  const plus = () => {
-    setTulos(numb + numbKaks)
+  const calculate = operator => {
+    const [num1, num2] = [Number(numb), Number(numbKaks)];
+
+    switch (operator) {
+      case '+':
+        setTulos(num1+num2)
+        break;
+
+      case '-':
+        setTulos(num1-num2)
+        break;
+    }
+    setNumb('');
+    setNumbKaks('');
+    initialFocus.current.focus();
   }
 
-  const miinus = () => {
-    setTulos(numb - numbKaks)
-  }
 
   return (
-    <View style={styles.test}>
+    <View style={styles.conatiner}>
 
       <View >
         <Text>Tulos: {tulos}</Text>
       </View>
       
-      <View style={styles.input}>
-        <TextInput style={styles.input} onChange={numb => setNumb(numb)} value={numb}/>
-
-        <TextInput style={styles.input} onChangeText={numbKaks => setNumbKaks(numbKaks)} value={numbKaks}/>
-      </View>
-
       <View>
-        <Button title="Plussa" style={styles.button} onPress={plus}/>
 
-        <Button title="Miinus" style={styles.button} onPress={miinus}/>
+        <TextInput style={styles.input} 
+        ref={initialFocus}
+        onChangeText={text => setNumb(text)} 
+        value={numb}
+        keyboardType='number-pad'
+        />
+
+        <TextInput style={styles.input} 
+        onChangeText={text => setNumbKaks(text)} 
+        value={numbKaks}
+        keyboardType='number-pad'
+        />
+
       </View>
+
+
+      <View style={styles.operators}>
+        
+        <View style={styles.buttons}>
+          <Button title="Plussa" 
+              onPress={() => calculate('+')}
+            />
+        </View>
+          
+        <View style={styles.buttons}>
+          <Button title="Miinus" 
+              onPress={() => calculate('-')}
+            />
+        </View>
+        
+      </View>
+
+      <StatusBar style="auto" />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  test: {
+  conatiner: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -46,8 +82,22 @@ const styles = StyleSheet.create({
   },
   
   input: {
-    width: 70,
+    width: 220,
     borderColor: 'green',
     borderWidth: 2,
+    padding: 20,
+    margin: 15,
   },
+
+  operators: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+
+  buttons: {
+    width: '20%',
+    borderColor: 'red',
+    borderWidth: 2,
+    margin: 10,
+  }
 });
